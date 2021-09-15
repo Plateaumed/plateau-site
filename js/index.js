e60c78895beaaf08dbab3234e88bfa7f5fd0808f
Oluwatoba <tohbyy@gmail.com>
2021-09-15 16:52:02 +0100

    reduce opacity of shadow on nav bar

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let sendBtn = document.getElementById('send-mail');
 let notifyText = document.getElementById('plateaumed-notify');
 document.addEventListener('scroll', () => {
     if(window.scrollY > 150){
-        nav.style.boxShadow = '.1rem .2rem .3rem rgba(0, 0, 0, .2)'
+        nav.style.boxShadow = '.1rem .2rem .3rem rgba(0, 0, 0, .08)'
     }else{
         nav.style.background = '#FFF';
         nav.style.boxShadow = 'none';
