b3be6a3f41932781c9b18051b90d19e90c0c45c7
= <=>
2022-05-10 17:24:55 +0100

    fix calendly issue

--- appointment/index.js
+++ appointment/index.js
@@ -1,30 +1,47 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const phone = document.getElementById('phone');
-const hospital= document.getElementById('hospital');
-const address = document.getElementById('address');
+const usernameElement = document.getElementById('username');
+const phoneElement = document.getElementById('phone');
+const hospitalElement= document.getElementById('hospital');
+const addressElement = document.getElementById('address');
+const calendlyName = "maratah7";
+const calendlyMeetingName = "30min";
 
 let usernameValue, phoneValue, hospitalValue, addressValue;
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
+	alert("holla");
+
 	if(!checkInputs()){
-		let usernameValue = username.value
-		let phoneValue = phone.value;
-		let hospitalValue = hospital.value;
-		let addressValue = address.value;
-		location.href = `https://calendly.com/maratah7/30min?name=${usernameValue}&back=1&month=2022-05`
+		let usernameValue = usernameElement.value
+		let phoneValue = phoneElement.value?.charAt(0) == 0 ? "234"+ phoneElement.value?.substr(1):phoneElement.value ;
+		let hospitalValue = hospitalElement.value;
+		let addressValue = addressElement.value;
+		const d = new Date();
+		let month = d.getMonth()+1;
+		let year = d.getFullYear()
+		location.href = `https://calendly.com/${calendlyName}/${calendlyMeetingName}?name=${usernameValue}&a3=${phoneValue}&a1=${hospitalValue}&a2=${addressValue}&back=1&month=${year}-${month}`
 	}
 });
 
 
 
+document.addEventListener("DOMContentLoaded", function(event) {
+	let url = new URL(location.href);
+	let params = new  URLSearchParams(url.search);
+	if(params.get("success") === 'true'){
+		const modal = document.getElementById("modal")
+		openModal(modal)
+	}
+
+});
+
 function checkInputs() {
 	// trim to remove the whitespaces
-	const usernameValue = username.value.trim();
-	const phoneValue = phone.value.trim();
-	const hospitalValue = hospital.value.trim();
-	const addressValue = address.value.trim();
+	const username = usernameElement.value.trim();
+	const phone = phoneElement.value.trim();
+	const hospital = hospitalElement.value.trim();
+	const address = addressElement.value.trim();
 	let isErrored= false;
 	if(usernameValue === '') {
 		isErrored = true;
@@ -37,9 +54,7 @@ function checkInputs() {
 		isErrored = true;
 
 		setErrorFor(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(phone), 'Phone number cannot be blank');
-	} else if (!phone(phoneValue)) {
-		setErrorFor(phone, 'Not a valid phonenumber');
-	} else {
+	}else {
 		setSuccessFor(phone);
 	}
 	
@@ -53,10 +68,7 @@ function checkInputs() {
 	
 	if(addressValue === '') {
 		isErrored = true;
-		setErrorFor(/^[a-zA-Z0-9\s,'-]*$/.test (address), 'address cannot be blank');
-	} else if(!address(addressValue)) {
-		isErrored = true;
-		setErrorFor(address, 'Incorrect address');
+		setErrorFor(/^[a-zA-Z0-9\s,'-]*$/.test(address), 'address cannot be blank');
 	} else{
 		setSuccessFor(address);
 	}
@@ -72,12 +84,14 @@ function setErrorFor(input, message) {
 
 function setSuccessFor(input) {
 	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
+	console.log(input, "input")
+	// formControl.className = 'form-control success';
 }
 	
-// function isPhone(phonenumber) {
-// 	return /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;(phonenumber);
-// }
+function isPhone(phonenumber) {
+	console.log(phonenumber.match(/^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/),"match");
+	return phonenumber?.match(/^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/);;
+}
 // function isAddress(address) {
 // 	return /^[a-zA-Z0-9\s,.'-]{3,}$/ ;(address);
 // }
