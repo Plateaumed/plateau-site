c9ff84c3ded8a56a01ce9bee486fcc96d165852b
MaratahNjoroge <maratah7@gmail.com>
2022-05-10 16:40:29 +0300

    WIP:Intergrate calendly

--- appointment/index.js
+++ appointment/index.js
@@ -1,49 +1,66 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
-const phonenumber = document.getElementById('phonenumber');
+const phone = document.getElementById('phone');
 const hospital= document.getElementById('hospital');
 const address = document.getElementById('address');
 
+let usernameValue, phoneValue, hospitalValue, addressValue;
+
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	
-	checkInputs();
+	if(!checkInputs()){
+		let usernameValue = username.value
+		let phoneValue = phone.value;
+		let hospitalValue = hospital.value;
+		let addressValue = address.value;
+		location.href = `https://calendly.com/maratah7/30min?name=${usernameValue}&back=1&month=2022-05`
+	}
 });
+
+
 
 function checkInputs() {
 	// trim to remove the whitespaces
 	const usernameValue = username.value.trim();
-	const phoneValue = phonenumber.value.trim();
+	const phoneValue = phone.value.trim();
 	const hospitalValue = hospital.value.trim();
 	const addressValue = address.value.trim();
-	
+	let isErrored= false;
 	if(usernameValue === '') {
+		isErrored = true;
 		setErrorFor(/^[A-Za-z\s]+$/.test(username), 'username cannot be blank');
 	} else {
 		setSuccessFor(username);
 	}
 	
 	if(phoneValue === '') {
-		setErrorFor(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(phonenumber), 'Phone number cannot be blank');
-	} else if (!isPhone(phoneValue)) {
-		setErrorFor(phonenumber, 'Not a valid phonenumber');
+		isErrored = true;
+
+		setErrorFor(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(phone), 'Phone number cannot be blank');
+	} else if (!phone(phoneValue)) {
+		setErrorFor(phone, 'Not a valid phonenumber');
 	} else {
-		setSuccessFor(phonenumber);
+		setSuccessFor(phone);
 	}
 	
 	if(hospitalValue === '') {
-		setErrorFor(hospital, 'hospital cannot be blank');
+		isErrored = true;
+
+		setErrorFor(/^[A-Za-z\s]+$/.test(hospital), 'hospital cannot be blank');
 	} else {
 		setSuccessFor(hospital);
 	}
 	
 	if(addressValue === '') {
+		isErrored = true;
 		setErrorFor(/^[a-zA-Z0-9\s,'-]*$/.test (address), 'address cannot be blank');
-	} else if(isAddress(addressValue)) {
+	} else if(!address(addressValue)) {
+		isErrored = true;
 		setErrorFor(address, 'Incorrect address');
 	} else{
 		setSuccessFor(address);
 	}
+	return isErrored;
 }
 
 function setErrorFor(input, message) {
